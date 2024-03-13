fb77fc0aca427496e71dd70d33467b27103fb01c
jose <21jose03.free@gmail.com>
2024-03-13 17:54:27 -0500

    Editando los endpoints

--- src/main/java/com/hackacode/gestionPaqSer/controller/PaqueteController.java
+++ src/main/java/com/hackacode/gestionPaqSer/controller/PaqueteController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/paquetes")
+@RequestMapping("/paquetes")
 @Slf4j
 public class PaqueteController {
 
--- src/main/java/com/hackacode/gestionPaqSer/controller/ServicioController.java
+++ src/main/java/com/hackacode/gestionPaqSer/controller/ServicioController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/servicios")
+@RequestMapping("/servicios")
 @Slf4j
 public class ServicioController {
 
