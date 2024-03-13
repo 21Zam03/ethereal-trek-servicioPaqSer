d81e5d0289fd695440cb85f89daedf9fded48ade
jose <21jose03.free@gmail.com>
2024-03-13 09:45:50 -0500

    Primera version

---
+++ src/main/java/com/hackacode/gestionPaqSer/GestionPaqSerApplication.java
@@ -0,0 +1,13 @@
+package com.hackacode.gestionPaqSer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPaqSerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPaqSerApplication.class, args);
+	}
+
+}
